1d45e0b4f95a1ed68939260b4db700d8dd69a1f1
petitlang <L2164753957@gmail.com>
2025-05-20 11:26:41 +0200

    Question 2

--- YuefanLIU_MouzhengLI_LianghongLI.py
+++ YuefanLIU_MouzhengLI_LianghongLI.py
@@ -24,4 +24,52 @@ df_reponses = pd.read_csv('YuefanLIU_MouzhengLI_LianghongLI.csv') if not pd.read
 df_reponses.loc[df_reponses['Question'] == 'q1a', 'Reponse1'] = n_villes
 df_reponses.loc[df_reponses['Question'] == 'q1b', 'Reponse1'] = n_villes_nan
 
+# Trouver la ville et la valeur associée à chaque extrême demandé (q2a-h)
+# Pour chaque variable, on cherche la ville avec la valeur minimale et maximale
+
+def get_ville_val(df, col, mode='min'):
+    if mode == 'min':
+        idx = df[col].idxmin()
+    else:
+        idx = df[col].idxmax()
+    return df.loc[idx, 'Ville'], df.loc[idx, col]
+
+# Température minimale minimale (q2a)
+ville_2a, val_2a = get_ville_val(df_data1_clean, 'Temperature_minimale', 'min')
+# Température minimale maximale (q2b)
+ville_2b, val_2b = get_ville_val(df_data1_clean, 'Temperature_minimale', 'max')
+# Température maximale minimale (q2c)
+ville_2c, val_2c = get_ville_val(df_data1_clean, 'Temperature_maximale', 'min')
+# Température maximale maximale (q2d)
+ville_2d, val_2d = get_ville_val(df_data1_clean, 'Temperature_maximale', 'max')
+# Précipitations minimales (q2e)
+ville_2e, val_2e = get_ville_val(df_data1_clean, 'Hauteur_precipitations', 'min')
+# Précipitations maximales (q2f)
+ville_2f, val_2f = get_ville_val(df_data1_clean, 'Hauteur_precipitations', 'max')
+# Ensoleillement minimal (q2g)
+ville_2g, val_2g = get_ville_val(df_data1_clean, 'Duree_ensoleillement', 'min')
+# Ensoleillement maximal (q2h)
+ville_2h, val_2h = get_ville_val(df_data1_clean, 'Duree_ensoleillement', 'max')
+
+# Écrire les résultats dans le fichier de réponses
+# Les colonnes Reponse1 pour la ville, Reponse2 pour la valeur
+reps = [
+    ('q2a', ville_2a, val_2a),
+    ('q2b', ville_2b, val_2b),
+    ('q2c', ville_2c, val_2c),
+    ('q2d', ville_2d, val_2d),
+    ('q2e', ville_2e, val_2e),
+    ('q2f', ville_2f, val_2f),
+    ('q2g', ville_2g, val_2g),
+    ('q2h', ville_2h, val_2h),
+]
+for q, ville, val in reps:
+    df_reponses.loc[df_reponses['Question'] == q, 'Reponse1'] = ville
+    df_reponses.loc[df_reponses['Question'] == q, 'Reponse2'] = val
+
 df_reponses.to_csv('YuefanLIU_MouzhengLI_LianghongLI.csv', index=False)
+
+# Explication :
+# On cherche pour chaque variable la ville qui présente la valeur extrême (min ou max),
+# puis on enregistre le nom de la ville et la valeur correspondante dans le fichier de réponses.
+# Cela permet d'identifier les villes les plus chaudes, froides, pluvieuses ou ensoleillées de l'échantillon.
